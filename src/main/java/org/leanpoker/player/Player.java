266b453b9bc1dd59d4f4b8a8d0a013974c315f99
Mark Ungvari <ungvarimark@gmail.com>
2019-09-02 11:00:31 +0200

    FIrst push

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,8 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -9,7 +11,19 @@ public class Player {
     static final String VERSION = "Java 0.1";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        JsonObject json = request.getAsJsonObject();
+        JsonArray communityCards = json.get("community_cards").getAsJsonArray();
+        int current_buy_in = json.get("current_buy_in").getAsInt();
+        JsonArray players = json.get("players").getAsJsonArray();
+        int in_action = json.get("in_action").getAsInt();
+
+        JsonObject myPlayer = players.get(in_action).getAsJsonObject();
+        int bet = myPlayer.get("bet").getAsInt();
+
+
+        return current_buy_in - bet;
+
+
     }
 
     public static void showdown(JsonElement game) {
