8a6fd825e8edf09dbb9e5efd0fac40d435f79e1c
Mark Ungvari <ungvarimark@gmail.com>
2019-09-02 11:23:31 +0200

    Added comments and the raise option

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -16,14 +16,15 @@ public class Player {
         int current_buy_in = json.get("current_buy_in").getAsInt();
         JsonArray players = json.get("players").getAsJsonArray();
         int in_action = json.get("in_action").getAsInt();
-
+        int minimum_raise = json.get("minimum_raise").getAsInt();
         JsonObject myPlayer = players.get(in_action).getAsJsonObject();
-        int bet = myPlayer.get("bet").getAsInt();
+        int ourBet = myPlayer.get("bet").getAsInt();
 
+        // This is the call method
+        int check = current_buy_in - ourBet;
+        int raise = current_buy_in - ourBet + minimum_raise;
 
-        return current_buy_in - bet;
-
-
+        return check;
     }
 
     public static void showdown(JsonElement game) {
