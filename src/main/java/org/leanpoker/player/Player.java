6b745ade97c469f4256ae18a3d44a3e9de720306
Márton Tévald <marton.tevald@gmail.com>
2019-09-02 10:40:28 +0200

    buy_in and players extract, wip

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -13,6 +13,15 @@ public class Player {
     public static int betRequest(JsonElement request) {
         JsonObject json = request.getAsJsonObject();
         JsonArray communityCards = json.get("community_cards").getAsJsonArray();
+        int current_buy_in = json.get("current_buy_in").getAsInt();
+        JsonArray players = json.get("players").getAsJsonArray();
+        int in_action = json.get("in_action").getAsInt();
+
+        int bet = json.get("bet").getAsInt();
+
+
+
+        current_buy_in - players[in_action][bet];
 
 
         return 0;
